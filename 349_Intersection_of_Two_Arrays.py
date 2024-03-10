04ed859fd38309a300a7b8ef2ae1f06e183dad18
CostiaB <elveco@gmail.com>
2024-03-10 11:29:36 -0300

    349. Intersection of Two Arrays

---
+++ 349_Intersection_of_Two_Arrays.py
@@ -0,0 +1,64 @@
+"""
+349. Intersection of Two Arrays
+
+Given two integer arrays nums1 and nums2, return an array of their intersection.
+ Each element in the result must be unique and you may return the result in any order.
+
+
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+
+
+
+Constraints:
+
+    1 <= nums1.length, nums2.length <= 1000
+    0 <= nums1[i], nums2[i] <= 1000
+
+"""
+
+class Solution:
+    def intersection(self, nums1: list[int], nums2: list[int]) -> list[int]:
+        cnt = {}
+        for n in nums1:
+            cnt[n] = 1 + cnt.get(n, 0)
+
+        res = []
+        for n in nums2:
+            if n in cnt:
+                res.append(n)
+                del cnt[n]
+        return res
+
+test0 = {
+    'input':{ 'nums1':[1, 2, 2, 1],
+              'nums2':[2, 2]
+    },
+    'output': [2]
+}
+
+test1 = {
+    'input': {'nums1': [4, 9, 5],
+              'nums2': [9, 4, 9, 8, 4]
+    },
+    'output': [9, 4]
+}
+
+tests = [test0, test1]
+
+for test in tests:
+    print(Solution().intersection(**test['input']) == test['output'])
+
+"""
+Runtime 45ms Beats74.57% of users with Python3
+Memory 16.80MB Beats25.10% of users with Python3
+"""
