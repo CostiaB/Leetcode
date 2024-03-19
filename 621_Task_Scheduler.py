284844e0461f7a70172cf531b60f7fed13922a68
CostiaB <elveco@gmail.com>
2024-03-19 10:06:56 -0300

    621. Task Scheduler

---
+++ 621_Task_Scheduler.py
@@ -0,0 +1,98 @@
+"""
+621. Task Scheduler
+
+You are given an array of CPU tasks, each represented by letters A to Z, and a cooling time, n. Each cycle or interval
+ allows the completion of one task. Tasks can be completed in any order, but there's a constraint: identical tasks
+ must be separated by at least n intervals due to cooling time.
+
+Return the minimum number of intervals required to complete all tasks.
+
+ Example 1:
+
+Input: tasks = ["A","A","A","B","B","B"], n = 2
+
+Output: 8
+
+Explanation: A possible sequence is: A -> B -> idle -> A -> B -> idle -> A -> B.
+
+After completing task A, you must wait two cycles before doing A again. The same applies to task B. In the 3rd interval,
+ neither A nor B can be done, so you idle. By the 4th cycle, you can do A again as 2 intervals have passed.
+
+Example 2:
+
+Input: tasks = ["A","C","A","B","D","B"], n = 1
+
+Output: 6
+
+Explanation: A possible sequence is: A -> B -> C -> D -> A -> B.
+
+With a cooling interval of 1, you can repeat a task after just one other task.
+
+Example 3:
+
+Input: tasks = ["A","A","A", "B","B","B"], n = 3
+
+Output: 10
+
+Explanation: A possible sequence is: A -> B -> idle -> idle -> A -> B -> idle -> idle -> A -> B.
+
+There are only two types of tasks, A and B, which need to be separated by 3 intervals. This leads to idling twice
+between repetitions of these tasks.
+
+Constraints:
+
+    1 <= tasks.length <= 104
+    tasks[i] is an uppercase English letter.
+    0 <= n <= 100
+
+"""
+import heapq
+from collections import deque, Counter
+
+
+class Solution:
+    def leastInterval(self, tasks: list[str], n: int) -> int:
+        # each task 1 unit of time
+        # minimize idle time
+        cnt = Counter(tasks)
+        maxHeap = [-count for count in cnt.values()]
+        heapq.heapify(maxHeap)
+        time = 0
+        q = deque() # pairs of [-count, idleTime]
+
+        while maxHeap or q:
+            time += 1
+            if maxHeap:
+                count = 1 + heapq.heappop(maxHeap)
+                if count:
+                    q.append([count, time + n])
+
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap, q.popleft()[0])
+        return time
+
+
+test0 = {
+    'input': {'tasks': ["A", "C", "A", "B", "D", "B"],
+              'n': 1
+
+    },
+    'output': 6
+}
+test1 = {
+    'input': {'tasks': ["A", "A", "A", "B", "B", "B"],
+              'n': 3
+
+    },
+    'output': 10
+}
+
+tests = [test0, test1]
+
+for test in tests:
+    print(Solution().leastInterval(**test['input']) == test['output'])
+
+"""
+Runtime 483ms Beats 35.08% of users with Python3
+Memory 17.00MB Beats 79.06% of users with Python3
+"""
