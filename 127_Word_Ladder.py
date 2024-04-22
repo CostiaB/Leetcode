26f7c3af6c008b2645af6a6c30ab48d5f9dd99c2
CostiaB <elveco@gmail.com>
2024-04-22 15:48:11 -0300

    127. Word Ladder

--- 127_Word_Ladder.py
+++ 127_Word_Ladder.py
@@ -9,7 +9,7 @@ words beginWord -> s1 -> s2 -> ... -> sk such that:
     sk == endWord
 
 Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest
-transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+transformation sequence from begin  Word to endWord, or 0 if no such sequence exists.
 
 Example 1:
 Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
